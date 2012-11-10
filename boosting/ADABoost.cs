bf3e374024edeb5250a404e58b1d2ed2f073c58f
Julius Buset Asplin <julius_asplin@hotmail.com>
2012-11-10 19:26:23 +0100

    ol

--- boosting/ADABoost.cs
+++ boosting/ADABoost.cs
@@ -16,15 +16,15 @@ namespace boosting
             List<double> w = new List<double>();
             for (int i = 0; i < N; i++)
 			{
-			    w.Add(1/N);
+			    w.Add((double)1/N);
 			}
             List<double> z = new List<double>();
-            for (int m = 1; m < M; m++)
+            for (int m = 0; m < M; m++)
             {
-                h[m] = L(examples, w);
+                h.Add(L(examples, w));
                 double error = 0;
                 for (int j = 0; j < N; j++)
-                    if (h[m].classify(examples[j].attributes) != examples[j].classification) error += + w[j];
+                    if (h[m].classify(examples[j].attributes) != examples[j].classification) error += w[j];
 
                 for (int j = 0; j < N; j++)
                     if (h[m].classify(examples[j].attributes) == examples[j].classification) w[j] *= error / (1 - error);
@@ -32,10 +32,35 @@ namespace boosting
                 double wTotal = w.Sum();
                 for (int j = 0; j < N; j++) w[m] /= wTotal;
                 
+
                 h[m].setWeight(Math.Log(error / (1- error)));
             }
 
             return h;
+        }
+
+        public static double test(List<Hypotheses> hypotheses, List<Case> testSet)
+        {
+            double tse = 0;
+            foreach (Case c in testSet)
+            {
+                tse += Math.Pow((classify(hypotheses, c.attributes) - c.classification), 2);
+            }
+            Console.WriteLine(testSet.Count);
+            Console.WriteLine(tse);
+            return tse / testSet.Count;
+        }
+
+        private static double classify(List<Hypotheses> hypotheses, List<double> attributes)
+        {
+            double classification = 0;
+            foreach (Hypotheses h in hypotheses)
+            {
+                classification += h.classify(attributes) * h.weight;
+                Console.WriteLine(h.weight);
+                Console.WriteLine(h.classify(attributes));
+            }
+            return classification;
         }
     }
 }
--- boosting/Program.cs
+++ boosting/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Algorithms;
 using System.IO;
+using System.Globalization;
 
 namespace boosting
 {
@@ -14,24 +15,21 @@ namespace boosting
         
         static void Main(string[] args)
         {
+            trainAndTest("yeast.txt");
             
-            
-            List<Hypotheses> id3 = ADABoost.weightedMajorityHypotheses(trainingSet, ID3.generateHypothesis, 10);
-            //List<Hypotheses> naiveBayes = ADABoost.weightedMajorityHypotheses(trainingSet, NaiveBayes.generateHypothesis, 10);
-
             Console.ReadLine();
         }
 
-        Tuple<List<Case>, List<Case>> getCaseSetsFromFile(String fileName)
+        static Tuple<List<Case>, List<Case>> getCaseSetsFromFile(String fileName)
         {
-            var reader = new StreamReader(File.OpenRead(@location + fileName));
+            var reader = new StreamReader(File.OpenRead(@"..\..\..\datasets\" + fileName));
             List<string> valueNames = reader.ReadLine().Split(',').ToList();
             List<Case> cases = new List<Case>();
             while (!reader.EndOfStream)
             {
-                List<double> attributes = reader.ReadLine().Split(',').Select(s => double.Parse(s)).ToList();
+                List<double> attributes = reader.ReadLine().Split(',').Select(s => double.Parse(s, CultureInfo.InvariantCulture)).ToList();
                 double classification = attributes.Last();
-                attributes.RemoveAt(-1);
+                attributes.RemoveAt(attributes.Count - 1);
                 Case c = new Case(attributes, classification);
                 cases.Add(c);
             }
@@ -43,6 +41,18 @@ namespace boosting
             testSet.AddRange(cases.Skip((int)(cases.Count * 0.8)));
 
             return new Tuple<List<Case>, List<Case>>(trainingSet, testSet);
+        }
+
+        static void trainAndTest(string fileName)
+        {
+            Console.WriteLine(fileName);
+            Tuple<List<Case>, List<Case>> caseSets = getCaseSetsFromFile(fileName);
+            
+            List<Hypotheses> id3 = ADABoost.weightedMajorityHypotheses(caseSets.Item1, ID3.generateHypothesis, 10);
+            Console.WriteLine("ID3: " + ADABoost.test(id3, caseSets.Item2));
+            
+            //List<Hypotheses> naiveBayes = ADABoost.weightedMajorityHypotheses(caseSets.Item1, NaiveBayes.generateHypothesis, 10);
+
         }
     }
 }
